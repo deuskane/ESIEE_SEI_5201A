711d11842aab55cb605e46ded297e19104c3b4a9
deuskane <deuskane@gmail.com>
2025-01-05 09:15:29 +0100

    labo04 : update init script

--- labo04/init.sh
+++ labo04/init.sh
@@ -1,12 +1,45 @@
+#!/bin/bash
 
-# Get Repository
-git clone https://github.com/deuskane/asylum-soc-OB8_gpio.git
+function init_main()
+{
+    local repo_work=asylum-soc-OB8_gpio
+    local repo_cores=asylum-cores
+    
+    # Header
+    echo ""
+    echo "Run initialization script for $(basename ${PWD})"
+    echo ""
 
-# Create directory if unexist
-mkdir -p ~/.config/fusesoc/
+    # Get Repository
+    if test -d ${repo_work};
+    then
+	echo "Directory \"${repo_work}\" exists, exit the script"
+	return
+    fi
 
-# Remove previous clone repo
-rm -fr /home/user/.local/share/fusesoc/asylum-cores
+    git clone https://github.com/deuskane/${repo_work}.git
 
-# Add global Library
-fusesoc library add asylum-cores https://github.com/deuskane/asylum-cores.git --global
+    # Re-create directory if unexist
+    rm -fr   ~/.config/fusesoc/
+    mkdir -p ~/.config/fusesoc/
+
+    # Remove previous clone repo
+    rm -fr   ~/.local/share/fusesoc/${repo_cores}
+
+    # Add global Library
+    fusesoc library add ${repo_cores} https://github.com/deuskane/${repo_cores}.git --global
+
+    # Add local Library
+    cd ${repo_work}
+    rm -f fusesoc.conf
+    fusesoc library add local .
+
+    # Check Core list
+    make info
+
+    echo ""
+    echo "Initialization complete"
+    echo ""
+}
+
+init_main $*
