ede24c62430ad640b8311f2ee2a84a934453df93
deuskane <deuskane@gmail.com>
2025-12-03 14:56:21 +0100

    labo01 : add empty model

---
+++ labo01/src/labo01.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std   .all;
+
+entity labo01 is
+  
+end entity labo01;
+
+architecture rtl of labo01 is
+
+begin  -- architecture rtl
+
+end architecture rtl;
