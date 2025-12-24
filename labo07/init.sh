9d281070ce132e9730934b7c923ab24ba58e1a52
deuskane <deuskane@gmail.com>
2025-12-24 06:36:31 +0100

    labo 4 to 8 : update init script

--- labo07/init.sh
+++ labo07/init.sh
@@ -2,31 +2,40 @@
 
 function init_main()
 {
-    local repo_work=asylum-soc-OB8_gpio
-    local repo_cores=asylum-cores
+    local work_repo=asylum-soc-picosoc
+    local cores_repo=asylum-cores
     
     # Header
-    echo ""
+    echo "--------------------------------------------------"
     echo "Run initialization script for $(basename ${PWD})"
-    echo ""
+    echo "--------------------------------------------------"
 
     # Get Repository
-    if test -d ${repo_work};
+    if test -d ${work_repo};
     then
-	echo "Directory \"${repo_work}\" exists, exit the script"
+	echo "Directory \"${work_repo}\" exists, exit the script"
 	return
     fi
 
-    cp -r ../labo06/${repo_work} .
+    # Get current dir
+    dirname_current=$(basename "$PWD")
+    dirname_current_prefix=${dirname_current//[0-9]/}
+    dirname_current_number=$(echo $dirname_current | grep -oE '[0-9]+')
+
+    # Get previous dir
+    dirname_previous_number=$(printf "%02d" $((10#$dirname_current_number - 1)))
+    dirname_previous="${dirname_current_prefix}${dirname_previous_number}"
+
+    cp -r ../${dirname_previous}/${work_repo} .
 
     # Add local Library
-    cd ${repo_work}
+    cd ${work_repo}
     rm -f fusesoc.conf
     fusesoc library add local .
 
     # Clean
     make clean
-    
+
     # Check Core list
     make info
 
